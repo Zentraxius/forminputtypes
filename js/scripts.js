2d7f35a32219f4cb42c3738897d5a99c3327481b
Cody Fritz <clanalia55@gmail.com>
2020-06-03 15:50:02 -0700

    add initial file to github to save information on form types

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+const beverage = $("beverage").val();
+// // // // //
+//   \/\/   //
+// // // // //
+const flavor = $("input:radio[name=flavor]:checked").val();
+// // // // //
+//   \/\/   //
+// // // // //
+const dob = $("#born").val();
+// // // // //
+//   \/\/   //
+// // // // //
+const favoriteColor = $("#color").val();
